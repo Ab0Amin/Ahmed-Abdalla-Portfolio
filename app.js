4591a324cb4ae52ff2e30ba48c747c406b979fd0
Ahmed Amin <ahmed.amin.abd.elwadod@gmail.com>
2023-04-13 12:31:04 +0200

    start my protoflio

---
+++ app.js
@@ -0,0 +1,13 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("snav").style.width = "200px";
+
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("snav").style.width = "0";
+}
+
+
+// how to print to log ?
