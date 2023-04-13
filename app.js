94a8191b83c3a8a0fb6b7516d8b47d17323a4649
Ahmed Amin <ahmed.amin.abd.elwadod@gmail.com>
2023-04-13 14:12:22 +0200

    add logo and drop list for mobile view

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("snav").style.width = "200px";
+    document.getElementById("snav").style.width = "100%";
+    document.getElementById("openbtn").style.visibility = "hidden";
+    document.getElementById("closebtn").style.visibility = "visible";
 
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("snav").style.width = "0";
+    document.getElementById("openbtn").style.visibility = "visible";
+    document.getElementById("closebtn").style.visibility = "hidden";
+
 }
 
 
-// how to print to log ?
+
+
+jQuery(window).on('scroll', function () {
+    if (jQuery(window).scrollTop() > 50) {
+        jQuery('#header_frame').css('background-color', 'white');
+        jQuery('#logo').css('filter', ' invert(0%)');
+    } else {
+        jQuery('#header_frame').css('background-color', '#323232');
+        jQuery('#logo').css('filter', ' invert(100%)');
+    }
+});
