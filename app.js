36c29b4430a6e262573208d74a91d0bd3219e748
Ahmed Amin <ahmed.amin.abd.elwadod@gmail.com>
2023-07-10 10:22:15 +0200

    fix bugs

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 /* Set the width of the side navigation to 250px */
+
+var opended = false
 function openNav() {
     document.getElementById("snav").style.width = "100%";
     document.getElementById("openbtn").style.visibility = "hidden";
     document.getElementById("closebtn").style.visibility = "visible";
     jQuery('.header_left').css('background-color', 'white');
-
+    jQuery('.header_left a').css('color', 'black');
+    jQuery('#header_frame').css('background-color', 'white');
+    jQuery('.headrCtr').css('background-color', 'white');
+    jQuery('#logo').css('filter', ' invert(0%)');
+opended=true
 }
 
 /* Set the width of the side navigation to 0 */
@@ -12,6 +18,7 @@ function closeNav() {
     document.getElementById("snav").style.width = "0";
     document.getElementById("openbtn").style.visibility = "visible";
     document.getElementById("closebtn").style.visibility = "hidden";
+    opended=false
 
 }
 
@@ -20,20 +27,24 @@ function closeNav() {
 //  color = "#323232";
 color = "local";
 jQuery(window).on('scroll', function () {
-    if (jQuery(window).scrollTop() > 50) {
-        jQuery('#header_frame').css('background-color', 'white');
-        jQuery('.headrCtr').css('background-color', 'white');
+    if(!opended)
+    {
 
-        jQuery('.header_left').css('background-color', 'white');
-        jQuery('.header_left a').css('color', 'black');
-        jQuery('#logo').css('filter', ' invert(0%)');
-
-    } else {
-        jQuery('#header_frame').css('background', color);
-        jQuery('.headrCtr').css('background', color);
-        jQuery('.header_left').css('background', color);
-        jQuery('.header_left a').css('color', 'white');
-        jQuery('#logo').css('filter', ' invert(100%)');
+        if (jQuery(window).scrollTop() > 50) {
+            jQuery('#header_frame').css('background-color', 'white');
+            jQuery('.headrCtr').css('background-color', 'white');
+            
+            jQuery('.header_left').css('background-color', 'white');
+            jQuery('.header_left a').css('color', 'black');
+            jQuery('#logo').css('filter', ' invert(0%)');
+            
+        } else {
+            jQuery('#header_frame').css('background', color);
+            jQuery('.headrCtr').css('background', color);
+            jQuery('.header_left').css('background', color);
+            jQuery('.header_left a').css('color', 'white');
+            jQuery('#logo').css('filter', ' invert(100%)');
+        }
     }
 });
 
